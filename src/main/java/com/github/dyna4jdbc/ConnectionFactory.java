c39daac7d5d6625ddf923fd12800cc95a0451e46
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2016-03-24 12:07:47 +0100

    Decoupled class DynaDriver from internal implementation details

--- src/main/java/com/github/dyna4jdbc/ConnectionFactory.java
+++ src/main/java/com/github/dyna4jdbc/ConnectionFactory.java
@@ -1,65 +1,91 @@
 package com.github.dyna4jdbc;
 
+import com.github.dyna4jdbc.internal.MisconfigurationSQLException;
+import com.github.dyna4jdbc.internal.SQLError;
+import com.github.dyna4jdbc.internal.common.util.exception.ExceptionUtil;
 import com.github.dyna4jdbc.internal.processrunner.jdbc.impl.ProcessRunnerConnection;
 import com.github.dyna4jdbc.internal.scriptengine.jdbc.impl.ScriptEngineConnection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
 import java.util.Properties;
 
 class ConnectionFactory {
 
-    private static final ConnectionFactory INSTANCE = new ConnectionFactory(); // thread-safe: no state is held
+	private static final ConnectionFactory INSTANCE = new ConnectionFactory(); // thread-safe: no state is held
 
 
-    static ConnectionFactory getInstance() {
-        return INSTANCE;
-    }
+	static ConnectionFactory getInstance() {
+		return INSTANCE;
+	}
 
-    java.sql.Connection newConnection(String bridgeName, String config, Properties info) throws Exception {
-    	try {
-    		BuiltInConnectionBridge connectionBridge = BuiltInConnectionBridge.getByName(bridgeName);
+	java.sql.Connection newConnection(String factoryConfiguration, Properties info) throws SQLException {
+		try {     
+			String[] bridgeNameAndConfig = factoryConfiguration.split(":", 2);
 
-            final Class<?> classToInstantiate = (connectionBridge != null) ? connectionBridge.clazz :
-                    Class.forName(bridgeName);
+			String bridgeName = bridgeNameAndConfig[0];
+			String config = bridgeNameAndConfig.length == 2 ? bridgeNameAndConfig[1] : null;
 
-            Constructor<?> constructor = classToInstantiate.getConstructor(String.class, Properties.class);
+			return newConnection(bridgeName, config, info);
+		}
+		catch (MisconfigurationSQLException ex)
+		{
+			String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
+			throw SQLError.INVALID_CONFIGURATION.raiseException(ex, causeMessage);
+		}
+		catch (Exception ex)
+		{
+			String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
+			throw SQLError.CONNECT_FAILED_GENERIC.raiseException(ex, causeMessage);
+		}
+	}
 
-            return (java.sql.Connection) constructor.newInstance(config, info);
-    	}
-    	catch(InvocationTargetException ite) {
-    		Throwable cause = ite.getCause();
+
+	private java.sql.Connection newConnection(String bridgeName, String config, Properties info) throws Exception {
+		try {
+			BuiltInConnectionBridge connectionBridge = BuiltInConnectionBridge.getByName(bridgeName);
+
+			final Class<?> classToInstantiate = (connectionBridge != null) ? connectionBridge.clazz :
+				Class.forName(bridgeName);
+
+			Constructor<?> constructor = classToInstantiate.getConstructor(String.class, Properties.class);
+
+			return (java.sql.Connection) constructor.newInstance(config, info);
+		}
+		catch(InvocationTargetException ite) {
+			Throwable cause = ite.getCause();
 			if (cause instanceof RuntimeException)
-				  throw (RuntimeException) cause;
-			  else if (cause instanceof Error)
-			      throw (Error) cause;
-			  else
-			      throw (Exception)cause;
-    	}
-   }
-    
-    private enum BuiltInConnectionBridge {
-        SCRIPTENGINE("scriptengine", ScriptEngineConnection.class),
-    	PROCESS_RUNNER("process-runner", ProcessRunnerConnection.class);
-    	
-        private final String name;
-        private final Class<? extends java.sql.Connection> clazz;
+				throw (RuntimeException) cause;
+			else if (cause instanceof Error)
+				throw (Error) cause;
+			else
+				throw (Exception)cause;
+		}
+	}
 
-        BuiltInConnectionBridge(String name, Class<? extends java.sql.Connection> clazz) {
-            this.name = name;
-            this.clazz = clazz;
-        }
+	private enum BuiltInConnectionBridge {
+		SCRIPTENGINE("scriptengine", ScriptEngineConnection.class),
+		PROCESS_RUNNER("process-runner", ProcessRunnerConnection.class);
 
-        private static BuiltInConnectionBridge getByName(String name) {
-            for (BuiltInConnectionBridge bridge : BuiltInConnectionBridge.values()) {
-                if (bridge.name.equalsIgnoreCase(name)) {
-                    return bridge;
-                }
-            }
+		private final String name;
+		private final Class<? extends java.sql.Connection> clazz;
 
-            return null;
-        }
-    }
+		BuiltInConnectionBridge(String name, Class<? extends java.sql.Connection> clazz) {
+			this.name = name;
+			this.clazz = clazz;
+		}
+
+		private static BuiltInConnectionBridge getByName(String name) {
+			for (BuiltInConnectionBridge bridge : BuiltInConnectionBridge.values()) {
+				if (bridge.name.equalsIgnoreCase(name)) {
+					return bridge;
+				}
+			}
+
+			return null;
+		}
+	}
 
 
 }
--- src/main/java/com/github/dyna4jdbc/DynaDriver.java
+++ src/main/java/com/github/dyna4jdbc/DynaDriver.java
@@ -1,99 +1,84 @@
 
 package com.github.dyna4jdbc;
 
-import com.github.dyna4jdbc.internal.MisconfigurationSQLException;
-import com.github.dyna4jdbc.internal.SQLError;
-import com.github.dyna4jdbc.internal.common.util.exception.ExceptionUtil;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
 import java.util.logging.Logger;
+
+import com.github.dyna4jdbc.internal.SQLError;
 
 
 public class DynaDriver implements java.sql.Driver {
 
-    public static final int DRIVER_VERSION_MAJOR = 0;
-    public static final int DRIVER_VERSION_MINOR = 1;
+	public static final int DRIVER_VERSION_MAJOR = 0;
+	public static final int DRIVER_VERSION_MINOR = 5;
 
-    public static final String DRIVER_SHORT_NAME = "Dyna4JDBC";
-    public static final String DRIVER_NAME = String.format("%s Dynamic Languages For JDBC Driver", DRIVER_SHORT_NAME);
-    public static final String DRIVER_VERSION = String.format("%s.%s", DRIVER_VERSION_MAJOR, DRIVER_VERSION_MINOR);
-
-
-    private static final String JDBC_URL_PREFIX = "jdbc:dyna4jdbc:";
+	public static final String DRIVER_SHORT_NAME = "Dyna4JDBC";
+	public static final String DRIVER_NAME = String.format("%s Dynamic Languages For JDBC Driver", DRIVER_SHORT_NAME);
+	public static final String DRIVER_VERSION = String.format("%s.%s", DRIVER_VERSION_MAJOR, DRIVER_VERSION_MINOR);
 
 
-    private static final Logger LOGGER = Logger.getLogger(DynaDriver.class.getPackage().getName());
+	private static final String JDBC_URL_PREFIX = "jdbc:dyna4jdbc:";
 
 
-    static {
-        try {
-            DriverManager.registerDriver(new DynaDriver());
-        }
-        catch (SQLException ex) {
-            throw new RuntimeException("Could not register to DriverManager: " + DynaDriver.class.getName(), ex);
-        }
-    }
-
-    private final ConnectionFactory connectionFactory;
-
-    public DynaDriver() {
-        this(ConnectionFactory.getInstance());
-    }
-
-    DynaDriver(ConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-    }
-
-    public Connection connect(String url, Properties info) throws SQLException {
-        try {
-            if (!acceptsURL(url)) SQLError.CONNECT_FAILED_INVALID_URL.raiseException(url);
-
-            String[] bridgeNameAndConfig = url.replace(JDBC_URL_PREFIX, "").split(":", 2);
-
-            String bridgeName = bridgeNameAndConfig[0];
-            String config = bridgeNameAndConfig.length == 2 ? bridgeNameAndConfig[1] : null;
-
-            return doConnect(bridgeName, config, info);
-        }
-        catch (MisconfigurationSQLException ex)
-        {
-        	String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
-            throw SQLError.INVALID_CONFIGURATION.raiseException(ex, causeMessage);
-        }
-        catch (Exception ex)
-        {
-        	String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
-            throw SQLError.CONNECT_FAILED_GENERIC.raiseException(ex, causeMessage);
-        }
-    }
-
-    protected Connection doConnect(String name, String config, Properties info) throws Exception {
-        return connectionFactory.newConnection(name, config, info);
-    }
+	private static final Logger LOGGER = Logger.getLogger(DynaDriver.class.getPackage().getName());
 
 
-    public boolean acceptsURL(String url) throws SQLException {
-        return url != null && url.startsWith(JDBC_URL_PREFIX);
-    }
+	static {
+		try {
+			DriverManager.registerDriver(new DynaDriver());
+		}
+		catch (SQLException ex) {
+			throw new RuntimeException("Could not register to DriverManager: " + DynaDriver.class.getName(), ex);
+		}
+	}
 
-    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
-        return new DriverPropertyInfo[0];
-    }
+	private final ConnectionFactory connectionFactory;
 
-    public int getMajorVersion() {
-        return DRIVER_VERSION_MAJOR;
-    }
+	public DynaDriver() {
+		this(ConnectionFactory.getInstance());
+	}
 
-    public int getMinorVersion() {
-        return DRIVER_VERSION_MINOR;
-    }
+	DynaDriver(ConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
 
-    public boolean jdbcCompliant() {
-        return false;
-    }
+	public Connection connect(String url, Properties info) throws SQLException {
 
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return LOGGER;
-    }
+		if (!acceptsURL(url)) {
+			SQLError.CONNECT_FAILED_INVALID_URL.raiseException(url);
+		}
+
+		String factoryConfiguration = url.replace(JDBC_URL_PREFIX, "");
+		
+		return connectionFactory.newConnection(factoryConfiguration, info);
+	}
+
+	public boolean acceptsURL(String url) throws SQLException {
+		return url != null && url.startsWith(JDBC_URL_PREFIX);
+	}
+
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+		return new DriverPropertyInfo[0];
+	}
+
+	public int getMajorVersion() {
+		return DRIVER_VERSION_MAJOR;
+	}
+
+	public int getMinorVersion() {
+		return DRIVER_VERSION_MINOR;
+	}
+
+	public boolean jdbcCompliant() {
+		return false;
+	}
+
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return LOGGER;
+	}
 }
