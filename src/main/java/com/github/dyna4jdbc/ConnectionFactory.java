04cfab94015634f3449fd1db1b0ac86ab0735faf
peter-gergely-horvath <peter.gergely.horvath@gmail.com>
2016-05-11 20:03:08 +0200

    Code quality enhancements

--- src/main/java/com/github/dyna4jdbc/ConnectionFactory.java
+++ src/main/java/com/github/dyna4jdbc/ConnectionFactory.java
@@ -8,84 +8,74 @@ import com.github.dyna4jdbc.internal.scriptengine.jdbc.impl.ScriptEngineConnecti
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.util.*;
 
 class ConnectionFactory {
 
-	private static final ConnectionFactory INSTANCE = new ConnectionFactory(); // thread-safe: no state is held
+    private static final ConnectionFactory INSTANCE = new ConnectionFactory(); // thread-safe: no state is held
+
+    private static final Map<String, Class<? extends Connection>> CONNECTION_TYPES;
+
+    static {
+        Map<String, Class<? extends Connection>> connectionTypes = new HashMap<>();
+
+        connectionTypes.put("scriptengine", ScriptEngineConnection.class);
+        connectionTypes.put("process-runner", ProcessRunnerConnection.class);
+
+        CONNECTION_TYPES = Collections.unmodifiableMap(connectionTypes);
+    }
 
 
-	static ConnectionFactory getInstance() {
-		return INSTANCE;
-	}
+    static ConnectionFactory getInstance() {
+        return INSTANCE;
+    }
 
-	java.sql.Connection newConnection(String factoryConfiguration, Properties info) throws SQLException {
-		try {     
-			String[] bridgeNameAndConfig = factoryConfiguration.split(":", 2);
+    java.sql.Connection newConnection(String factoryConfiguration, Properties info) throws SQLException {
+        try {
+            String[] bridgeNameAndConfig = factoryConfiguration.split(":", 2);
 
-			String bridgeName = bridgeNameAndConfig[0];
-			String config = bridgeNameAndConfig.length == 2 ? bridgeNameAndConfig[1] : null;
-
-			return newConnection(bridgeName, config, info);
-		}
-		catch (MisconfigurationSQLException ex)
-		{
-			String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
-			throw JDBCError.INVALID_CONFIGURATION.raiseSQLException(ex, causeMessage);
-		}
-		catch (Exception ex)
-		{
-			String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
-			throw JDBCError.CONNECT_FAILED_GENERIC.raiseSQLException(ex, causeMessage);
-		}
-	}
+            String bridgeName = bridgeNameAndConfig[0].toLowerCase(Locale.ENGLISH);
+            String config = bridgeNameAndConfig.length == 2 ? bridgeNameAndConfig[1] : null;
 
 
-	private java.sql.Connection newConnection(String bridgeName, String config, Properties info) throws Exception {
-		try {
-			BuiltInConnectionBridge connectionBridge = BuiltInConnectionBridge.getByName(bridgeName);
+            return newConnection(bridgeName, config, info);
 
-			final Class<?> classToInstantiate = (connectionBridge != null) ? connectionBridge.clazz :
-				Class.forName(bridgeName);
+        } catch (MisconfigurationSQLException ex) {
+            String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
+            throw JDBCError.INVALID_CONFIGURATION.raiseSQLException(ex, causeMessage);
+        } catch (Exception ex) {
+            String causeMessage = ExceptionUtil.getRootCauseMessage(ex);
+            throw JDBCError.CONNECT_FAILED_GENERIC.raiseSQLException(ex, causeMessage);
+        }
+    }
 
-			Constructor<?> constructor = classToInstantiate.getConstructor(String.class, Properties.class);
 
-			return (java.sql.Connection) constructor.newInstance(config, info);
-		}
-		catch(InvocationTargetException ite) {
-			Throwable cause = ite.getCause();
-			if (cause instanceof RuntimeException)
-				throw (RuntimeException) cause;
-			else if (cause instanceof Error)
-				throw (Error) cause;
-			else
-				throw (Exception)cause;
-		}
-	}
+    private java.sql.Connection newConnection(String connectionType, String config, Properties info) throws Exception {
+        try {
 
-	private enum BuiltInConnectionBridge {
-		SCRIPTENGINE("scriptengine", ScriptEngineConnection.class),
-		PROCESS_RUNNER("process-runner", ProcessRunnerConnection.class);
+            Class<? extends Connection> connectionClass = CONNECTION_TYPES.get(connectionType);
+            if (connectionClass == null) {
+                throw MisconfigurationSQLException.forMessage("No such connection type: '%s'", connectionType);
+            }
 
-		private final String name;
-		private final Class<? extends java.sql.Connection> clazz;
+            Constructor<? extends Connection> connectionConstructor =
+                    connectionClass.getConstructor(String.class, Properties.class);
 
-		BuiltInConnectionBridge(String name, Class<? extends java.sql.Connection> clazz) {
-			this.name = name;
-			this.clazz = clazz;
-		}
+            return connectionConstructor.newInstance(config, info);
 
-		private static BuiltInConnectionBridge getByName(String name) {
-			for (BuiltInConnectionBridge bridge : BuiltInConnectionBridge.values()) {
-				if (bridge.name.equalsIgnoreCase(name)) {
-					return bridge;
-				}
-			}
 
-			return null;
-		}
-	}
+        } catch (InvocationTargetException ite) {
+            Throwable cause = ite.getCause();
+            if (cause instanceof RuntimeException)
+                throw (RuntimeException) cause;
+            else if (cause instanceof Error)
+                throw (Error) cause;
+            else
+                throw (Exception) cause;
+        }
+    }
 
 
 }
