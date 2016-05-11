f4efb0a705602664303e6f2046540a1fe6993b6c
peter-gergely-horvath <peter.gergely.horvath@gmail.com>
2016-05-11 19:37:16 +0200

    Added new test class and the required Maven configuration

---
+++ src/test/java/com/github/dyna4jdbc/DriverTest.java
@@ -0,0 +1,135 @@
+package com.github.dyna4jdbc;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+import static org.testng.Assert.*;
+
+
+/**
+ * @author Peter Horvath
+ */
+public class DriverTest {
+
+    private static final String MAVEN_PROPERTIES_LOCATION =
+            "/test.maven.properties";
+
+
+    private final Properties mavenPropertiesCapturedDuringBuild = new Properties();
+
+    @BeforeClass
+    public void beforeClass() throws IOException {
+
+        try(InputStream is =
+                    DriverTest.class.getResourceAsStream(MAVEN_PROPERTIES_LOCATION)) {
+            if(is == null) throw new IllegalStateException("Not found: " + MAVEN_PROPERTIES_LOCATION);
+
+            mavenPropertiesCapturedDuringBuild.load(is);
+        }
+    }
+
+
+    @Test
+    public void testDriverLoadAndConnectionAcquisition() throws SQLException {
+
+        try (Connection connection = DriverManager.getConnection("jdbc:dyna4jdbc:scriptengine:JavaScript");) {
+
+            assertNotNull(connection);
+        }
+    }
+
+    @Test
+    public void testDriverClass() throws SQLException {
+
+        Driver driver = DriverManager.getDriver("jdbc:dyna4jdbc:scriptengine:JavaScript");
+
+        assertTrue(driver instanceof DynaDriver);
+    }
+
+    @Test
+    public void testDriverAcceptsCorrectUrl() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        assertTrue(driver.acceptsURL("jdbc:dyna4jdbc:scriptengine:JavaScript"));
+    }
+
+    @Test
+    public void testDriverDoesNotAcceptsInvalidUrl() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        assertFalse(driver.acceptsURL("jdbc:foo:bar"));
+    }
+
+    @Test
+    public void testDriverDoesNotThrowNPEFromAcceptsMethod() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        assertFalse(driver.acceptsURL(null));
+    }
+
+    @Test
+    public void testDriverOnlyAcceptsTerminatedUrl() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        assertTrue(driver.acceptsURL("jdbc:dyna4jdbc:"));
+        assertFalse(driver.acceptsURL("jdbc:dyna4jdbc"));
+    }
+
+    @Test
+    public void testDriverReportsAsNotJdbcCompliant() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        assertFalse(driver.jdbcCompliant());
+    }
+
+    @Test
+    public void testDriverReportsValidMajorVersionNumber() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        String projectVersion = mavenPropertiesCapturedDuringBuild.getProperty("project.version");
+
+        String majorVersionString = projectVersion.split("\\.")[0];
+
+        int majorVersion = driver.getMajorVersion(); // -SNAPSHOT
+
+        assertEquals(majorVersionString, Integer.toString(majorVersion));
+    }
+
+    @Test
+    public void testDriverReportsValidMinorVersionNumber() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        String projectVersion = mavenPropertiesCapturedDuringBuild.getProperty("project.version");
+
+        String minorVersionString = projectVersion.split("\\.")[1].replace("-SNAPSHOT", "");
+
+        int minorVersion = driver.getMinorVersion();
+
+        assertEquals(minorVersionString, Integer.toString(minorVersion));
+    }
+
+    @Test
+    public void testDriverReturnsParentLogger() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        assertNotNull(driver.getParentLogger());
+    }
+
+    @Test
+    public void testDriverPropertyInforIsNotEmpty() throws SQLException {
+        DynaDriver driver = new DynaDriver();
+
+        DriverPropertyInfo[] propertyInfo =
+                driver.getPropertyInfo("jdbc:dyna4jdbc:scriptengine:JavaScript", new Properties());
+
+        assertNotNull( propertyInfo );
+        assertTrue( propertyInfo.length > 0 );
+    }
+
+
+}
