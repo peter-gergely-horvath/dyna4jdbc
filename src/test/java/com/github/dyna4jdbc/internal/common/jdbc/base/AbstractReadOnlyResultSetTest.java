182a9f9b08a01f9cd74a58b6293461a5ca5f050f
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2016-06-03 22:28:30 +0200

    Code cleanup in tests

--- src/test/java/com/github/dyna4jdbc/internal/common/jdbc/base/AbstractReadOnlyResultSetTest.java
+++ src/test/java/com/github/dyna4jdbc/internal/common/jdbc/base/AbstractReadOnlyResultSetTest.java
@@ -42,7 +42,7 @@ public class AbstractReadOnlyResultSetTest {
     }
 
 
-    private AbstractReadOnlyResultSet abstractReadOnlyResultSet;
+    private AbstractReadOnlyResultSet<?> abstractReadOnlyResultSet;
 
     @BeforeMethod
     public void beforeMethod()  {
