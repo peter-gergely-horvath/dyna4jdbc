557ad632175b8c3106ef4c66fdd3f0ff77807db4
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2017-02-07 07:30:55 +0100

    Added license headers

--- src/main/java/com/github/dyna4jdbc/internal/nodejs/jdbc/impl/NodeJsProcessScriptExecutorFactory.java
+++ src/main/java/com/github/dyna4jdbc/internal/nodejs/jdbc/impl/NodeJsProcessScriptExecutorFactory.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2016, 2017 Peter G. Horvath, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
 package com.github.dyna4jdbc.internal.nodejs.jdbc.impl;
 
 import java.sql.SQLWarning;
@@ -28,7 +45,7 @@ public final class NodeJsProcessScriptExecutorFactory extends DefaultExternalPro
                 + "require('repl').start({ " 
                 + "terminal: false, " + "prompt: '', " 
                 + "ignoreUndefined: true, "
-                + "eval: function(cmd, ctx, fn, cb) { " 
+                + "eval: function(cmd, ctx, fn, cb) { "
                 + "try { vm.runInContext(cmd, ctx, fn); } "
                 + "catch (err) { cb(err); } "
                 + "finally { console.log(endOfStreamToken); console.error(endOfStreamToken); } "
--- src/main/java/com/github/dyna4jdbc/internal/processrunner/jdbc/impl/ProcessManagerFactory.java
+++ src/main/java/com/github/dyna4jdbc/internal/processrunner/jdbc/impl/ProcessManagerFactory.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2016, 2017 Peter G. Horvath, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
 package com.github.dyna4jdbc.internal.processrunner.jdbc.impl;
 
 import java.util.concurrent.ExecutorService;
@@ -19,7 +36,7 @@ final class ProcessManagerFactory {
         return new ProcessManagerFactory(configuration, executorService);
     }
     
-    
+
     ProcessManager newProcessManager(Process process) throws ProcessExecutionException {
         return ProcessManager.newInstance(process, configuration, executorService);
     }
