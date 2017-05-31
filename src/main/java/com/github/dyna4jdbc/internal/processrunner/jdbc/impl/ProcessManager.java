1c6d87be7a2b85f97ba2cc1497032e9763a78ea6
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2017-05-31 21:38:14 +0200

    Code cleanup

--- src/main/java/com/github/dyna4jdbc/internal/processrunner/jdbc/impl/ProcessManager.java
+++ src/main/java/com/github/dyna4jdbc/internal/processrunner/jdbc/impl/ProcessManager.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
- 
+
 package com.github.dyna4jdbc.internal.processrunner.jdbc.impl;
 
 import java.io.BufferedReader;
@@ -30,6 +30,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
@@ -43,34 +44,30 @@ final class ProcessManager {
         void onLineRead(String line);
     }
 
-    private static final int DEFAULT_POLL_INTERVAL_MS = 500;
-
     private static final Logger LOGGER = Logger.getLogger(ProcessManager.class.getName());
 
-    private static final int DEFAULT_TIMEOUT_MILLI_SECONDS = 10_000;
+    private static final long DEFAULT_POLL_INTERVAL_MS = 500;
+    private static final long DEFAULT_TIMEOUT_MILLI_SECONDS = 10_000;
+
+    private static final String END_OF_STREAM_INDICATOR_TOKEN = UUID.randomUUID().toString();
+
 
     private final Process processReference;
 
+
     private final ExecutorService executorService;
-    
+
+
     private final PrintWriter processInputWriter;
 
     private final BlockingQueue<String> standardOutputStreamContentQueue;
     private final BlockingQueue<String> errorStreamContentQueue;
 
-    private final String endOfStreamIndicator;
-
     private final Configuration configuration;
 
-    private boolean standardOutReachedEnd = false;
-    private boolean standardErrorReachedEnd = false;
 
-    
-    static ProcessManager newInstance(
-            Process process,
-            Configuration configuration,
-            ExecutorService executorService)
-                    throws ProcessExecutionException {
+    static ProcessManager newInstance(Process process, Configuration configuration, ExecutorService executorService)
+            throws ProcessExecutionException {
 
         try {
             ProcessManager processManager = new ProcessManager(process, configuration, executorService);
@@ -84,8 +81,7 @@ final class ProcessManager {
              */
             CyclicBarrier cyclicBarrier = new CyclicBarrier(partiesToWait);
 
-            DefaultIOHandlerFactory ioHandlerFactory = DefaultIOHandlerFactory.getInstance(configuration);
-
+            IOHandlerFactory ioHandlerFactory = DefaultIOHandlerFactory.getInstance(configuration);
 
             BufferedReader stdOut = ioHandlerFactory.newBufferedReader(
                     processManager.processReference.getInputStream());
@@ -95,13 +91,11 @@ final class ProcessManager {
 
             Runnable stdOutReader = new BufferedReaderToBlockingQueueRunnable(
                     "StdOut reader", stdOut,
-                    processManager.standardOutputStreamContentQueue, cyclicBarrier,
-                    processManager.endOfStreamIndicator);
+                    processManager.standardOutputStreamContentQueue, cyclicBarrier);
 
             Runnable stdErrReader = new BufferedReaderToBlockingQueueRunnable(
                     "StdErr reader", stdErr,
-                    processManager.errorStreamContentQueue, cyclicBarrier,
-                    processManager.endOfStreamIndicator);
+                    processManager.errorStreamContentQueue, cyclicBarrier);
 
             processManager.executorService.execute(stdOutReader);
             processManager.executorService.execute(stdErrReader);
@@ -119,49 +113,31 @@ final class ProcessManager {
         }
     }
 
-    private ProcessManager(
-            Process process,
-            Configuration configuration,
-            ExecutorService executorService)
-                    throws ProcessExecutionException {
+    private ProcessManager(Process process, Configuration configuration, ExecutorService executorService)
+            throws ProcessExecutionException {
 
+        this.configuration = configuration;
+        this.processReference = process;
 
-            this.configuration = configuration;
+        IOHandlerFactory ioHandlerFactory = DefaultIOHandlerFactory.getInstance(configuration);
+        processInputWriter = ioHandlerFactory.newPrintWriter(process.getOutputStream(), true);
 
-            Pattern endOfDataPattern = configuration.getEndOfDataPattern();
-            if (endOfDataPattern != null) {
-                this.endOfStreamIndicator = endOfDataPattern.toString();
-            } else {
-                this.endOfStreamIndicator = UUID.randomUUID().toString();
-            }
+        standardOutputStreamContentQueue = new LinkedBlockingQueue<>();
+        errorStreamContentQueue = new LinkedBlockingQueue<>();
 
-            this.processReference = process;
-
-            IOHandlerFactory ioHandlerFactory = DefaultIOHandlerFactory.getInstance(configuration);
-
-            processInputWriter = ioHandlerFactory.newPrintWriter(process.getOutputStream(), true);
-
-            standardOutputStreamContentQueue = new LinkedBlockingQueue<>();
-            errorStreamContentQueue = new LinkedBlockingQueue<>();
-
-            this.executorService = executorService;
-    }
-
-    private void checkProcessState() {
-
-        if (!isProcessRunning()) {
-            throw new IllegalStateException("Process is not running");
-        }
-    }
-
-    Future<Void> submitReadTaskForStdErr(ReadListener onStdErr) {
-        return executorService.submit(
-                new StdErrorWatcher(configuration, onStdErr));
+        this.executorService = executorService;
     }
 
     Future<Void> submitReadTaskForStdOut(ReadListener onStdOut) {
         return executorService.submit(
-                new StdOutWatcher(configuration, onStdOut));
+                new OutputListenerDispatcher(standardOutputStreamContentQueue,
+                        configuration.getSkipFirstLine(), configuration, onStdOut));
+    }
+
+    Future<Void> submitReadTaskForStdErr(ReadListener onStdErr) {
+        return executorService.submit(
+                new OutputListenerDispatcher(errorStreamContentQueue,
+                        false, configuration, onStdErr));
     }
 
     boolean isProcessRunning() {
@@ -169,59 +145,10 @@ final class ProcessManager {
     }
 
     void terminateProcess() {
-        checkProcessState();
-
         processReference.destroyForcibly();
     }
 
-    private boolean isStandardOutReachedEnd() {
-        return standardOutReachedEnd;
-    }
-
-    private String pollStandardOutput(long timeout, TimeUnit unit) throws IOException {
-
-        try {
-            String result = standardOutputStreamContentQueue.poll(timeout, unit);
-
-            if (endOfStreamIndicator.equals(result)) {
-                this.standardOutReachedEnd = true;
-                result = null;
-            }
-
-            return result;
-
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new IOException(e);
-        }
-    }
-
-    private boolean isStandardErrorReachedEnd() {
-        return standardErrorReachedEnd;
-    }
-
-    private String pollStandardError(long timeout, TimeUnit unit) throws IOException {
-
-        try {
-            String result = errorStreamContentQueue.poll(timeout, unit);
-
-            if (endOfStreamIndicator.equals(result)) {
-                this.standardErrorReachedEnd = true;
-                result = null;
-            }
-
-            return result;
-
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new IOException(e);
-        }
-    }
-
     void writeToStandardInput(String string) {
-
-        checkProcessState();
-
         processInputWriter.println(string);
         processInputWriter.flush();
     }
@@ -231,18 +158,16 @@ final class ProcessManager {
         private final BufferedReader bufferedReader;
         private final BlockingQueue<String> blockingQueue;
         private CyclicBarrier cyclicBarrier;
-        private final String endOfStreamIndicator;
 
-        private BufferedReaderToBlockingQueueRunnable(String identifier, BufferedReader bufferedReader,
+        private BufferedReaderToBlockingQueueRunnable(String identifier,
+                                                      BufferedReader bufferedReader,
                                                       BlockingQueue<String> blockingQueue,
-                                                      CyclicBarrier cyclicBarrier,
-                                                      String endOfStreamIndicator) {
+                                                      CyclicBarrier cyclicBarrier) {
 
             this.identifier = identifier;
             this.bufferedReader = bufferedReader;
             this.blockingQueue = blockingQueue;
             this.cyclicBarrier = cyclicBarrier;
-            this.endOfStreamIndicator = endOfStreamIndicator;
         }
 
         @Override
@@ -253,32 +178,23 @@ final class ProcessManager {
                 while (!Thread.currentThread().isInterrupted()) {
                     String line = bufferedReader.readLine();
                     if (line == null) {
-                        blockingQueue.put(endOfStreamIndicator);
+                        blockingQueue.put(END_OF_STREAM_INDICATOR_TOKEN);
                         break;
                     }
                     blockingQueue.put(line);
                 }
-            } catch (IOException e) {
+            } catch (IOException | BrokenBarrierException | TimeoutException e) {
                 throw new RuntimeException(e);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-            } catch (BrokenBarrierException e) {
-                LOGGER.warning("CyclicBarrier is broken: " + identifier);
-                e.printStackTrace();
-                // abort execution
-            } catch (TimeoutException e) {
-                LOGGER.warning("CyclicBarrier timeout: " + identifier);
-                e.printStackTrace();
-                // abort execution
             } finally {
                 try {
                     if (bufferedReader != null) {
                         bufferedReader.close();
                     }
                 } catch (IOException e) {
-                    LOGGER.warning("IOException closing bufferedReader in: " + identifier);
-                    e.printStackTrace();
-                    // swallow any exception raised on close
+                    // should NEVER happen
+                    LOGGER.log(Level.WARNING, "Ignored exception thrown while closing BufferedReader", e);
                 }
             }
 
@@ -298,62 +214,23 @@ final class ProcessManager {
 
     }
 
-    private final class StdErrorWatcher implements Callable<Void> {
+    private static final class OutputListenerDispatcher implements Callable<Void> {
 
-        private final Pattern endOfDataPattern;
-        private final ReadListener listener;
-        private final long expirationIntervalMs;
-
-        StdErrorWatcher(Configuration configuration, ReadListener listener) {
-            this.expirationIntervalMs = configuration.getExternalCallQuietPeriodThresholdMs();
-            this.endOfDataPattern = configuration.getEndOfDataPattern();
-            this.listener = listener;
-
-        }
-
-        @Override
-        public Void call() throws Exception {
-            long expirationTime = System.currentTimeMillis() + expirationIntervalMs;
-
-            while (System.currentTimeMillis() < expirationTime) {
-
-                String outputCaptured = ProcessManager.this.pollStandardError(
-                        DEFAULT_POLL_INTERVAL_MS, TimeUnit.MILLISECONDS);
-
-                if (outputCaptured == null) {
-
-                    if (ProcessManager.this.isStandardErrorReachedEnd()
-                            || ProcessManager.this.isStandardOutReachedEnd()) {
-                        break;
-                    }
-
-                } else {
-
-                    if (endOfDataPattern != null
-                            && endOfDataPattern.matcher(outputCaptured).matches()) {
-                        break;
-                    }
-
-                    expirationTime = System.currentTimeMillis() + expirationIntervalMs;
-                    listener.onLineRead(outputCaptured);
-                }
-            }
-
-            return null;
-        }
-
-    }
-
-    private final class StdOutWatcher implements Callable<Void> {
-
+        private final BlockingQueue<String> queueToPoll;
         private final boolean skipFirstLine;
         private final Pattern endOfDataPattern;
         private final ReadListener listener;
         private final long expirationIntervalMs;
 
-        StdOutWatcher(Configuration configuration, ReadListener listener) {
+
+        OutputListenerDispatcher(
+                BlockingQueue<String> queueToPoll,
+                boolean skipFirstLine,
+                Configuration configuration,
+                ReadListener listener) {
+            this.queueToPoll = queueToPoll;
             this.expirationIntervalMs = configuration.getExternalCallQuietPeriodThresholdMs();
-            this.skipFirstLine = configuration.getSkipFirstLine();
+            this.skipFirstLine = skipFirstLine;
             this.endOfDataPattern = configuration.getEndOfDataPattern();
             this.listener = listener;
 
@@ -361,41 +238,29 @@ final class ProcessManager {
 
         @Override
         public Void call() throws Exception {
-            long expirationTime = System.currentTimeMillis() + expirationIntervalMs;
 
             boolean firstLine = true;
+            long expirationTime;
+            do {
 
-            while (System.currentTimeMillis() < expirationTime) {
+                String outputRead = queueToPoll.poll(DEFAULT_POLL_INTERVAL_MS, TimeUnit.MILLISECONDS);
 
-                String outputCaptured = ProcessManager.this.pollStandardOutput(
-                        DEFAULT_POLL_INTERVAL_MS, TimeUnit.MILLISECONDS);
-
-                if (outputCaptured == null) {
-
-                    if (ProcessManager.this.isStandardOutReachedEnd()) {
-                        break;
-                    }
+                if (outputRead == null || outputRead.equals(END_OF_STREAM_INDICATOR_TOKEN)
+                        || endOfDataPattern != null && endOfDataPattern.matcher(outputRead).matches()) {
+                    break; // reached end of stream: break out of the loop
 
                 } else {
 
                     expirationTime = System.currentTimeMillis() + expirationIntervalMs;
 
-                    if (endOfDataPattern != null
-                            && endOfDataPattern.matcher(outputCaptured).matches()) {
-                        break;
-                    }
-
-                    if (firstLine) {
+                    if (skipFirstLine && firstLine) {
                         firstLine = false;
-
-                        if (skipFirstLine) {
-                            continue;
-                        }
+                        continue;
                     }
 
-                    listener.onLineRead(outputCaptured);
+                    listener.onLineRead(outputRead);
                 }
-            }
+            } while (System.currentTimeMillis() < expirationTime);
 
             return null;
         }
